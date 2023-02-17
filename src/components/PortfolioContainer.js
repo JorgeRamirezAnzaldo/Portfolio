f163e9affd5a820256a5e5c68a18e770fb6674d7
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-02-16 22:53:08 -0600

    Adding logic to portfolio container

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -5,6 +5,26 @@ import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import Footer from './Footer';
+const apps = [
+  {
+    title: "Title 1",
+    appLink: "Link1",
+    appGithub: "Github1",
+    lesson: "Lesson1"
+  },
+  {
+    title: "Title 2",
+    appLink: "Link2",
+    appGithub: "Github2",
+    lesson: "Lesson2"
+  },
+  {
+    title: "Title 3",
+    appLink: "Link3",
+    appGithub: "Github3",
+    lesson: "Lesson3"
+  },
+];
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
@@ -14,12 +34,15 @@ export default function PortfolioContainer() {
       return <About />;
     }
     if (currentPage === 'Portfolio') {
-      return <Portfolio />;
+      return <Portfolio applications = {apps}/>;
     }
     if (currentPage === 'Contact') {
       return <Contact />;
     }
-    return <Resume />;
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
@@ -27,7 +50,7 @@ export default function PortfolioContainer() {
   return (
     <div>
       <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
-      {/*renderPage()*/}
+      {renderPage()}
       <Footer/>
     </div>
   );
