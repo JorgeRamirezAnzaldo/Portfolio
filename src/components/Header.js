18d79a99cbcb49f71d650b2d4013201c3fb5be2f
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-02-17 12:28:08 -0600

    Adding style to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import Navigation from './Navigation';
+import backgroundHeader from '../images/FondoPaginaWeb.png';
+
+const styles = {
+    header : {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        color: "white",
+        //backgroundImage: "linear-gradient(rgb(5, 61, 87, 0.6) 0%, rgb(5, 61, 87, 1) 100%",
+        background: `url(${backgroundHeader})`,
+        backgroundSize: "cover",
+        height: "300px"
+    },
+    heading: {
+        fontSize: "36px",
+        marginLeft: "40px",
+        backgroundColor:"rgb(36, 84, 100)"
+    },
+    nav: {
+        fontSize: "3rem",
+        marginLeft: "auto",
+        marginRight: "20px"
+    }
+}
 
 function Header({currentPage, handlePageChange}) {
     return (
-        <div>
-            <h1>Jorge Alejandro Ramírez Anzaldo</h1>
-            <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+        <div style = {styles.header}>
+            <h1 style = {styles.heading}>Jorge Alejandro Ramírez Anzaldo</h1>
+            <Navigation style = {styles.nav} currentPage={currentPage} handlePageChange={handlePageChange} />
         </div>
     );
 
