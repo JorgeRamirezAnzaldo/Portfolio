19e1a7db986fa955cd295d5a2e38a1b65a3b458a
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-02-17 12:26:23 -0600

    Adding style and photo to About me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import PersonalPhoto from '../../images/PersonalPhoto.jpeg';
+
+const styles = {
+    photo: {
+        height: "250px",
+        width: "200px",
+        marginTop: "20px"
+    },
+    heading: {
+        marginTop: "20px",
+        color: "white",
+        fontSize: "30px",
+        fontWeight: "bold"
+    },
+    text:{
+        marginTop: "20px",
+        color: "white",
+        fontSize: "24px"
+    }
+}
 
 function About() {
     return (
         <div>
-            <p> 
+            <h2 style={styles.heading}>About me</h2>
+            <img style={styles.photo} src={PersonalPhoto}></img>
+            <p style={styles.text}> 
                 I am a mechatronics engineer graduated from Instituto Tecnológico y de Estudios Superiores de Monterrey, Campus Estado de México. Currently working as
                         a SAP Consultant in an enterprise named Adcos de México. I am certified as an ABAP developer with SAP Netweaver 7.5. Also, I am certified as an Automator
                         using the Worsoft Certify program to automatize business processes performed using SAP and multiple connected interfaces. I am interested in automation,
