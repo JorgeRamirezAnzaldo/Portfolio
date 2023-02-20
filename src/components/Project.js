4972e62463a7d007842e3ab134b320c7b4417da0
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-02-20 01:09:15 -0600

    Modifying Portfolio page and Project component to properly show the applications with the desired styles effect

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 
+const styles = {
+  title:{
+    display: "inline-block",
+    marginRight: "10px",
+    fontSize: "28px",
+    color: "black"
+  },
+  lesson:{
+    fontSize: "22px",
+    color: "black",
+    marginLeft: "20px"
+  },
+  icon:{
+    marginBottom: "10px",
+    fill: "black"
+  },
+}
+
 function Project({application}) {
   return (
-    <section>
-        <a href={application.appLink} target="_blank">
-            <h3>{application.title}</h3>
+    <section className="appSection" style={{background: `url(${application.image})`, backgroundSize: "cover"}} > 
+      <div className = "appData">
+        <a className="appLink" href={application.appLink} target="_blank">
+            <h3  style={styles.title}>{application.title}</h3>
         </a>
-        <a href={application.appGithub}><FaGithub></FaGithub></a>
-        <h4>{application.lesson}</h4>
+        <a className="appLink" href={application.appGithub} ><FaGithub size={28} style={styles.icon}></FaGithub></a>
+        <h4 style={styles.lesson}>{application.lesson}</h4>
+      </div>
+      
+      
     </section>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import Project from './Project';
+import Project from '../Project';
+import '../../styles/Style.css';
+import editorImage from '../../images/Editor.JPG';
 
-function Portfolio(props) {
+const apps = [
+  {
+    title: "Title 1",
+    appLink: "Link1",
+    appGithub: "Github1",
+    lesson: "Lesson1",
+    image: editorImage
+  },
+  {
+    title: "Title 2",
+    appLink: "Link2",
+    appGithub: "Github2",
+    lesson: "Lesson2"
+  },
+  {
+    title: "Title 3",
+    appLink: "Link3",
+    appGithub: "Github3",
+    lesson: "Lesson3"
+  },
+  {
+    title: "Title 4",
+    appLink: "Link4",
+    appGithub: "Github4",
+    lesson: "Lesson4"
+  },
+  {
+    title: "Title 5",
+    appLink: "Link5",
+    appGithub: "Github5",
+    lesson: "Lesson5"
+  },
+  {
+    title: "Title 6",
+    appLink: "Link6",
+    appGithub: "Github6",
+    lesson: "Lesson6"
+  },
+];
+
+function Portfolio() {
   return (
-    <section>
+    <section className="projectContainer">
       {/* TODO: Utilice el metodo `map` para iterar sobre cada elemento `grocery` y regrese un elemento `li` que contenga el nombre del elemento y un atributo único*/}
-      {props.applications.map((application) => {
+      {apps.map((application) => {
         return (
           <Project application = {application}/>
         );
